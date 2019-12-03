880921240b577e77aad4ab5ac2e3bc431c84dfcd
Andres Ramos <andresramosn@hotmail.com>
2019-12-03 17:14:30 +0100

    redirect after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-    devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
     root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :users, only: [:show] do
@@ -10,4 +11,3 @@ Rails.application.routes.draw do
       end
   resources :cars, only: [:index, :show]
 end
-
