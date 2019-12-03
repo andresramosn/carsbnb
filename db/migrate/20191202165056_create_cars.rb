a9b775ef8c141c7a9f68a949e0e495597580c08d
Pablo Maisuls <pablo_maisuls@live.com>
2019-12-03 14:19:36 +0100

    Fix – fixed database

--- db/migrate/20191202165056_create_cars.rb
+++ db/migrate/20191202165056_create_cars.rb
@@ -2,8 +2,9 @@ class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
       t.integer :price
-      t.string :modelbrand
-      t.string :adress
+      t.string :model
+      t.string :brand
+      t.string :address
       t.string :description
       t.string :photo
       t.references :user, foreign_key: true
