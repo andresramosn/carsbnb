2951c97f2e037510c10f35a37f9e840851c83654
Pablo Maisuls <pablo_maisuls@live.com>
2019-12-03 16:38:13 +0100

    implemented add a car and sign in links

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :users, only: [:show] do
-      resources :cars, only: [:new, :create, :edit, :update, :destroy] do
-        resources :reservations, only: [:new, :create]
+      resources :cars, only: [:new, :create] do
+        # resources :reservations, only: [:new, :create]
         end
       end
   resources :cars, only: [:index, :show]
