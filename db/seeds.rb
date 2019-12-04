2166b1896763783b08935ff871d9e2ebf2201666
Andres Ramos <andresramosn@hotmail.com>
2019-12-04 12:37:42 +0100

    proper seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,30 @@ puts 'Cleaning database'
 Car.destroy_all
 User.destroy_all
 
-# restaurants
-puts 'Creating cars'
+# cars and users
+puts 'Creating users'
 
-5.times do |_x|
+
+  brand = ["Toyota", "Kia", "Renault", "Seat", "BMW", "Mercedes Benz"]
+  model = ["Corolla", "Sportage", "Clio", "Leon", "x1", "CLA"]
+  address = ["bailen 152, barcelona", "corsega 523, barcelona", "plaza real 100, barcelona", "mallorca 1432, barcelona", "manigua 542, barcelona", "proser del castro 122, barcelona"]
+  photo_path ="app/assets/images/"
+  photo = ["https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&auto=format&fit=crop&w=778&q=80", "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", "https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80", "https://images.unsplash.com/photo-1517994112540-009c47ea476b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1381&q=80", "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", "https://images.unsplash.com/photo-1514867644123-6385d58d3cd4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"]
+  description = ["4 Seats, 2 Doors, Air Conditioning, Manual gearbox", "Sporty car, 4 Seats, 2 Doors, Air Conditioning, Manual gearbox", "Sporty car, 4 Seats, 2 Doors, Air Conditioning, Automatic gearbox", "Classic car, 4 Seats, 4 Doors, Air Conditioning, Automatic gearbox", "Sporty car, 4 Seats, 4 Doors, Air Conditioning, Automatic gearbox", "Sporty car, 4 Seats, 4 Doors, Air Conditioning, Secuential gearbox"]
+
+counter = 0
+
+6.times do |_x|
   random_email = ("a".."z").to_a.sample(10).join + "@gmail.com"
   random_password = (1..99).to_a.sample(8).join
-  random_price = (1..9).to_a.sample(4).join.to_i
-  brand = ["Toyota", "Kia", "Renault", "Seat", "BMW"]
-  model = ["x1", "x2", "x3", "x4"]
-  address = ["bailen 152, barcelona", "corsega 523, barcelona", "plaza real 100", "mallorca 1432"]
-  #create cars
+  #create users
   user = User.create(email: random_email, password: random_password)
-  car = Car.create(price: random_price, model: model.sample, brand: brand.sample, address: address.sample, description: "lorem ipsum", photo: "image/upload/v1575393584/lmvvrz3kpo4qevcxm5ns.jpg", user_id: user.id)
+  #create cars
+  random_price = (1..9).to_a.sample(2).join.to_i
+  car = Car.new(price: random_price, model: model[counter], brand: brand[counter], address: address[counter], description: description[counter], user_id: user.id)
+  car.remote_photo_url = photo[counter]
+  car.save
+  counter += 1
 end
 
 puts "#{User.count} users created & #{Car.count} cars created"
