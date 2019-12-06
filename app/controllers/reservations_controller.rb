d7a4cea9e4a4cfd87b1a160e75c733682581654e
Inaki Mediavilla <imediabar@gmail.com>
2019-12-06 15:15:48 +0100

    fgkjl

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,10 +15,37 @@ class ReservationsController < ApplicationController
 
   def index
     @reservations = Reservation.all
+    @user_reservations = Reservation.where(user_id: current_user.id)
+    @user_cars = Car.where(user_id: current_user.id)
   end
 
   def show
     @reservation = Reservation.find(params[:id])
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reject
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = "rejected"
+    @reservation.save
+    redirect_to reservations_path
+  end
+
+  def cancel
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = "cancelled"
+    @reservation.save
+    redirect_to reservations_path
+  end
+
+  def confirm
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = "confirmed"
+    @reservation.save
+    redirect_to reservations_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-    root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :users, only: [:show] do
-      resources :cars, only: [:new, :create]
-        end
-    resources :cars, only: [:index, :show] do
-      resources :reservations, only: [:create]
+  root to: 'pages#home'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :cars, only: [:new, :create]
   end
-    resources :reservations, only: [:index, :show]
+  resources :cars, only: [:index, :show] do
+    resources :reservations, only: [:create, :edit]
+
+  end
+  resources :reservations, only: [:index, :show]
+  patch "reject/reservations/:id", to: "reservations#reject", as: "reject"
+  patch "confirm/reservations/:id", to: "reservations#confirm", as: "confirm"
+  patch "cancel/reservations/:id", to: "reservations#cancel", as: "cancel"
+
 end
